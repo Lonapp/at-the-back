be120791fdb7a7bc72580b0b5774d1e38652cf09
Kyle Ladd <kladd@barracuda.com>
2014-06-18 15:09:59 -0400

    Close #10: Keep 'id' as column name in db

--- app/base/models.py
+++ app/base/models.py
@@ -13,7 +13,7 @@ from app import db
 class BaseModel(db.Model):
     __abstract__  = True
 
-    object_id = db.Column(db.Integer, primary_key=True)
+    object_id = db.Column('id', db.Integer, primary_key=True)
     date_created = db.Column(db.DateTime,  default=db.func.current_timestamp())
     date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
 
