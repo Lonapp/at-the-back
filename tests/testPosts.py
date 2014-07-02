00642d1dd9b55b15a1010f083466c06c2f4a9620
Marco Botros <btsmarco@gmail.com>
2014-07-02 02:53:20 +0000

    figured out how to call the app from tests dir, to run just run 'python test_file.py'

--- tests/testPosts.py
+++ tests/testPosts.py
@@ -1,8 +1,26 @@
 #This is a test file for the model posts, for each method created in posts we
 #will create automatic tests that run here using nose. 
-from app.posts.models import Posts
+from flask import Flask, current_app
+import os, sys, unittest, inspect
 
-class TestPosts():
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+os.sys.path.insert(0,parentdir)
 
-    def __init__():
-        pass
+from app.posts.models import Post
+
+class TestPosts(unittest.TestCase):
+
+    def SetUp(self):
+        app = Flask(__name__)
+        with app.app_context():
+            current_app.config['Testing'] = True
+            return current_app
+
+
+    def test_app(self):
+        assert(True)
+
+
+if __name__ == "__main__":
+    unittest.main()
