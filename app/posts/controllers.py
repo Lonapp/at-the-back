b555ec78c6d27e9dea97caab813a137310d46674
Marco Botros <btsmarco@gmail.com>
2014-07-15 01:10:24 +0000

    Add stubs for all the API in posts

--- app/posts/controllers.py
+++ app/posts/controllers.py
@@ -16,12 +16,33 @@ def index():
 
 @posts.route('/<post_id>', methods = ['GET'])
 def getPost(post_id):
-    #db.session.query()
+    #TODO refactor
     return json.dumps([i.json for i in Post.query(Post).get(post_id)])
 
 
+@posts.route('/<post_id>/voters', methods = ['GET'])
+def getPostVoters(post_id):
+    pass
+
+@posts.route('/<post_id>/lonners', methods = ['GET'])
+def getPostLonners(post_id):
+    pass
+
+@posts.route('/<post_id>', methods = ['DELETE'])
+def deletePost(post_id):
+    #TODO refactor
+    #DO we delete all lonned posts too?
+    ThePost = Post.query(Post).get(post_id)
+    Post.delete(ThePost)
+    #see the count and return 200 OK if 0, 404 otherwise
+    return True
+
+@posts.route('/<post_id>/lon', methods = ['POST'])
+def lonPost(post_id):
+    pass
+
 @posts.route('/', methods = ['POST'])
-def store():
+def postPost():
     # Create new post
     post = Post(request.form['message'])
 
@@ -30,3 +51,32 @@ def store():
 
     # Respond with completed post object
     return json.dumps(post.json)
+
+@posts.route('/<post_id>/comments/<user_id>', methods = ['GET'])
+def getComment(post_id,user_id):
+    pass
+
+@posts.route('/<post_id>/comments/<user_id>/voters', methods = ['GET'])
+def getCommentVoters(post_id, user_id):
+    pass
+
+@posts.route('/<post_id>/comments', methods = ['PUT'])
+def editComment(post_id):
+    #see the user logged in and give them access if the post is their's
+    pass
+
+@posts.route('/<post_id>/comments/<user_id>', methods = ['DELETE'])
+def deleteComment(post_id, user_id):
+    pass
+
+
+@posts.route('/<post_id>/comments/<user_id>', methods = ['POST'])
+def postComment(post_id,user_id):
+    pass
+
+
+@posts.route('/<post_id>/comments/<user_id>/vote', methods = ['POST'])
+def postCommentVote(post_id,user_id):
+    pass
+
+
