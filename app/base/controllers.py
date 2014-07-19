2219f434ed035f21fb91526329fa04017e81ee74
George Stephanos <gaf.stephanos@gmail.com>
2014-07-19 03:46:38 +0200

    Added decorator that ensures all arguments are supplied

--- app/base/controllers.py
+++ app/base/controllers.py
@@ -4,3 +4,18 @@ from sqlalchemy.ext.serializer import dumps
 from sqlalchemy.orm import sessionmaker
 
 from app.base.models import BaseModel
+
+#a decorator that checks if all arguments are supplied
+class args():
+    def __init__(self,args):
+        self.args = args
+    def __call__(self,func):
+        def inner(**kwargs):
+            try:
+                for i in self.args:
+                    assert i in request.args, 'missing ' + i
+                result = func(**kwargs)
+            except AssertionError, e:
+                result = str(e),400
+            return result
+        return inner
